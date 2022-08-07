399e83903988ad2a5dae7a1dee2dac2260c534f6
Nedim Begic <nedim.begic93@gmail.com>
2022-08-07 22:58:07 +0200

    first commit

---
+++ js/timer_stopwatch.js
@@ -0,0 +1,7 @@
+
+/* Event Listeners on start, stop and restart */
+document.querySelector('#onlyStart').addEventListener('click', start);
+
+function start(){
+    document.querySelector('#onlyStart').style.backgroundColor = "blue";
+}
